865a05d704c57b05c9c7e2ea85f8e5967b3555d9
semailk <sem.alik@mail.ru>
2026-02-01 15:24:55 +0400

    01-02-2026

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserStoreRequest;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserStoreRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        $newUser = new User();
+        $newUser->name = $validated['name'];
+        $newUser->email = $validated['email'];
+        $newUser->password = $validated['password'];
+        $newUser->save();
+
+        return redirect()->back()->with('success', 'User created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return view('users.show', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::resource('users', UserController::class);
+
+//Route::prefix('users')->group(function () {
+//    Route::get('', [UserController::class, 'index'])->name('users.index');
+//    Route::get('{id}', [UserController::class, 'show'])->name('users.show');
+//    Route::post('', [UserController::class, 'store'])->name('users.store');
+//    Route::delete('{id}', [UserController::class, 'destroy'])->name('users.destroy');
+//    Route::patch('{id}', [UserController::class, 'update'])->name('users.update');
+//});
